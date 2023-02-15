5da3bbcf84f4107681c191c961fca4a28b0c3cf2
matheus321699 <71594615+matheus321699@users.noreply.github.com>
2023-02-15 20:00:45 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/com/github/matheus321699/StartupTests.java
@@ -0,0 +1,13 @@
+package com.github.matheus321699;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
